06965660e55f22c8ee8ebe2814c2ff7d72d4edfb
Seth Snel <sethsnel@gmail.com>
2024-03-10 12:27:04 +0100

    Initial setup

---
+++ frontend/astro.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig } from "astro/config";
+import { loadEnv } from "vite";
+import storyblok from "@storyblok/astro";
+import sitemap from "@astrojs/sitemap";
+
+const { STORYBLOCK_ACCESS_TOKEN } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://zelfplukken.nl",
+  integrations: [
+    sitemap(),
+    storyblok({
+      accessToken: STORYBLOCK_ACCESS_TOKEN,
+      components: {
+        page: "storyblok/page",
+        pagina_header: "storyblok/page-header",
+        paragraaf: "storyblok/paragraph",
+        index: "storyblok/index",
+        menu_item: "storyblok/menu-item-blok",
+        website_header: "storyblok/website-header",
+      },
+    }),
+  ],
+});
